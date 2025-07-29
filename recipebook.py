446e4ffb24afd4155a20aa4b55abbfcae72415e6
dottybusch <125220401+dottybusch@users.noreply.github.com>
2025-07-29 12:40:07 +0100

    File reading functions added

--- recipebook.py
+++ recipebook.py
@@ -5,9 +5,11 @@ class Ingredient:
 
 
 class Recipe:
-    def __init__(self):
+    def __init__(self,name):
         self.ingredients = {}
         self.method =''
+        self.name = name
+      
 
     def add_ingredient(self, ingredient, amount):
 
@@ -22,3 +24,25 @@ class Recipe:
     def print_ingredients(self):
         for key, value in self.ingredients.items():
                     print(key.name + ": " + str(value) + key.unit)
+
+    def write_recipe(self, file):
+        items = []
+        for key, value in Cake.ingredients.items():
+            items.append(key.name + ": " + str(value) + key.unit)
+        file = open(file, mode = "a+")
+        file.write("\n")
+        file.write("--- " + self.name.upper() + " ---")
+        file.write("\n\nINGREDIENTS\n__________________\n")
+        for item in items:
+            file.write(item)
+            file.write("\n")
+        file.write("\n\nMETHOD\n__________________\n")
+        file.write(self.method)
+        file.write("\n\n\n")
+        file.close()
+
+Sugar = Ingredient("Sugar", "g")
+Flour = Ingredient("Flour", "g")
+Eggs = Ingredient("Eggs", "")
+Milk = Ingredient("Milk", "ml")
+
